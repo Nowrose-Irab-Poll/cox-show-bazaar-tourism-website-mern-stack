21105d642f31babfad035407eb7e50eb8acecdd2
Nowrose-Irab-Poll <nowroseirabpoll@gmail.com>
2021-10-29 12:16:33 +0600

    gathered some fake data

---
+++ src/fakedata.js
@@ -0,0 +1,72 @@
+const sites = [
+  {
+    name: `Cox's Bazar Beach`,
+    description:
+      "The longest and probably the most beautiful sea beach in the world",
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/e9/bb/cc/sugandha-beach-cox-s.jpg?w=500&h=400&s=1`,
+  },
+  {
+    name: `Inani Beach`,
+    description: `The heart of Cox's Bazaar's beauty full of corals and wonders.`,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/55/99/4f/inani-beach.jpg?w=500&h=-1&s=1`,
+  },
+  {
+    name: `Marine Drive`,
+    description: `A quiet, long drive along the coast, with green coconuts and the ocean lapping away.`,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/db/f8/1c/marine-drive.jpg?w=500&h=-1&s=1`,
+  },
+  {
+    name: `Himchori`,
+    description: `Collection of hill, fountain and sea view together.`,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/9a/3d/f1/the-bay-of-bengle-from.jpg?w=500&h=400&s=1`,
+  },
+  {
+    name: `100 feet Buddha`,
+    description: `Buddha statue of 100 feet in siting posture looking at you right from the road it starts.`,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/99/50/f8/100-feet-buddha.jpg?w=500&h=400&s=1`,
+  },
+  {
+    name: `Laboni Beach`,
+    description: `Most popular beach of Cox's Bazaar with thousands of people enjoying their vacations everyday.`,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/d2/61/27/laboni-beach-seen-on.jpg?w=500&h=400&s=1`,
+  },
+];
+
+const packages = [
+  {
+    name: `Magical Cox`,
+    description: `3 Days, 2 Nights, 5 Star Hotel, including Breakfast, Pool Access, Gym and Boat Riding`,
+    price: 10000,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/ae/6c/21/entrance-bridge-of-mermaid.jpg?w=500&h=-1&s=1`,
+  },
+  {
+    name: `Best of Cox`,
+    description: `4 Days, 3 Nights, 5 Star Hotel, including Breakfast, Pool Access, Gym and Boat Riding`,
+    price: 12000,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/44/f5/1b/photo0jpg.jpg?w=500&h=400&s=1`,
+  },
+  {
+    name: `Grand Cox`,
+    description: `4 Days, 3 Nights, 5 Star Hotel, including Breakfast, Gala Dinner, Hiking, 1 day Site Seeing, Pool Access, Bar Access, Gym and Boat Riding`,
+    price: 20000,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/a2/b8/ad/this-is-a-very-nice-island.jpg?w=500&h=400&s=1`,
+  },
+  {
+    name: `Honeymoon Cox`,
+    description: `Honeymoon Suite, 3 Days, 2 Nights, 5 Star Hotel, including Breakfast, Pool Access, Gym and Boat Riding`,
+    price: 11000,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/16/70/97/mermaid-beach-resort.jpg?w=500&h=-1&s=1`,
+  },
+  {
+    name: `Bachelor Cox`,
+    description: `3 Days, 2 Nights, 3 Star Hotel, including Breakfast, Pool Access`,
+    price: 8000,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/e9/c9/13/cox-s-bazar-best-view.jpg?w=500&h=400&s=1`,
+  },
+  {
+    name: `Budget Cox`,
+    description: `2 Days, 1 Nights, 3 Star Hotel, including Breakfast`,
+    price: 6000,
+    img: `https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/55/65/87/caption.jpg?w=500&h=400&s=1`,
+  },
+];
